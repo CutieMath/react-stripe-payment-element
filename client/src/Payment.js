4be93e2b8ec8d643cae73358d3b4edbdcef77ccd
Yuxin Ye <yuxin_ye@protonmail.com>
2023-01-26 14:01:53 +1100

    create states for FE

--- client/src/Payment.js
+++ client/src/Payment.js
@@ -2,6 +2,18 @@ import { useEffect, useState } from "react";
 import { loadStripe } from "@stripe/stripe-js";
 
 function Payment(props) {
+  const [stripePromise, setStripePromise] = useState(null);
+  const [clientSecret, setClientSecret] = useState("");
+
+  useEffect(() => {
+    // First, get the public key from the server
+    fetch("/config").then(async (r) => {
+      const { publishableKey } = await r.json();
+      console.log(publishableKey);
+      setStripePromise(loadStripe(publishableKey));
+    });
+  }, []);
+
   return (
     <>
       <h1>React Stripe and the Payment Element</h1>
